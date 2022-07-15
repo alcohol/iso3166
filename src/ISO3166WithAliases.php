fec7c728599480e2b18423e2c57ccdee8b56e94f
8ctopus <hello@octopuslabs.io>
2022-07-15 11:37:42 +0500

    Added Czechia alias

--- src/ISO3166WithAliases.php
+++ src/ISO3166WithAliases.php
@@ -28,6 +28,7 @@ class ISO3166WithAliases implements ISO3166DataProvider
         $aliases = [
             'Bolivia' => 'Bolivia (Plurinational State of)',
             'Congo-Kinshasa' => 'Congo (Democratic Republic of the)',
+            'Czech Republic' => 'Czechia',
             'Iran' => 'Iran (Islamic Republic of)',
             'North Korea' => 'Korea (Democratic People\'s Republic of)',
             'South Korea' => 'Korea (Republic of)',
